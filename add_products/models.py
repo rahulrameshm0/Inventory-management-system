a7635c49bd12447ed02a9d3601dd569e1df352e8
rahulrameshm0 <iamrahulm23@gmail.com>
2026-01-04 01:18:15 +0530

    created models for adding products

--- add_products/models.py
+++ add_products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    PRODUCT_STATUS = [
+        ('IN STOCK', 'In stock'),
+        ('OUT OF STOCK', 'Out of stock'),
+    ]
+
+    PRODUCT_TYPES = [
+        ('electronics', 'Electronics'),
+        ('grocery', 'Grocery'),
+        ('clothing', 'Clothing'),
+    ]
+    product_id = models.IntegerField(unique=True)
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=20,choices=PRODUCT_STATUS)
+    product_types = models.CharField(max_length=20, choices=PRODUCT_TYPES)
+    vendor_name = models.CharField(max_length=150)
+
+
