671871a4707cc81a39c51a4590d61d76eab8e4bb
rahulrameshm0 <iamrahulm23@gmail.com>
2026-01-09 01:27:12 +0530

    js updated

--- static/js/script.js
+++ static/js/script.js
@@ -2,22 +2,38 @@ const button = document.querySelector('.btn');
 const subtract = document.querySelector('.minus');
 const add = document.querySelector('.plus');
 const input = document.querySelector('.qty-input');
-const alerting = document.querySelector('.alerting')
+const alerting = document.querySelector('.alerting');
+const dashboardButton = document.querySelector('.btn-1');
 
-button.addEventListener('click', () => {
-    button.textContent = "Loading..."
-    console.log('click')
+if(button){
+    button.addEventListener('click', (e) => {
+        e.preventDefault()
+        button.textContent = "Loading..."
+        console.log('click')
+    });
+}
+
+if (add && input){
+    add.addEventListener('click', (e) => {
+        e.preventDefault()
+        input.value = parseInt(input.value) + 1;
+    });
+}
+
+if (subtract && input){
+    subtract.addEventListener('click', (e) => {
+        e.preventDefault()
+        if (input.value > 0){
+            input.value = parseInt(input.value) - 1;
+        }
+    });
+
+}
+
+if (dashboardButton){
+    dashboardButton.addEventListener('click',(e) => {
+        e.preventDefault()
+        window.location.href = dashboardButton.dataset.url;
+        // console.log('click')
 });
-
-add.addEventListener('click', (e) => {
-    e.preventDefault()
-    input.value = parseInt(input.value) + 1;
-});
-
-subtract.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (input.value > 0){
-        input.value = parseInt(input.value) - 1;
-    }
-});
-
+}
