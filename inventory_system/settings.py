ca52f79a096b254ee220bcd488964e86dfe70747
rahulrameshm0 <iamrahulm23@gmail.com>
2026-01-12 18:19:11 +0530

    codbase updated

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,6 +84,17 @@ WSGI_APPLICATION = 'inventory_system.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+# DATABASES =     {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'SRhDgPoANxxqHyKZVmeXhjlIsNEJeiMe',
+#         'HOST': 'postgres.railway.internal',
+#         'PORT': '5432',
+#     }
+# }
 
 DATABASES = {
     'default': {
