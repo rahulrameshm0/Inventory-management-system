219b579e954600337f5b9f2232a63ed563783e6b
rahulrameshm0 <iamrahulm23@gmail.com>
2026-01-18 19:14:00 +0530

    img added

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.user, name='user')
+    path('', views.user, name='user'),
+    path('cart/', views.cart, name='cart'),
+    path('product_details/', views.product_details, name='product_details'),
 ]
--- user/views.py
+++ user/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def user(request):
     return render(request, 'user-dashboard.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def product_details(request):
+    return render(request, 'product-details.html')
