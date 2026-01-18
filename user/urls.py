675247220f874538067f46954250197b4b2fb155
rahulrameshm0 <iamrahulm23@gmail.com>
2026-01-18 22:07:21 +0530

    user page updated

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.user, name='user'),
     path('cart/', views.cart, name='cart'),
-    path('product_details/', views.product_details, name='product_details'),
+    path('product_details', views.product_details, name='product_details'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect, get_object_or_404
+from add_products.models import Products
 # Create your views here.
 def user(request):
     return render(request, 'user-dashboard.html')
@@ -7,5 +7,6 @@ def user(request):
 def cart(request):
     return render(request, 'cart.html')
 
-def product_details(request):
-    return render(request, 'product-details.html')
+def product_details(request, id):
+    product =  get_object_or_404(id=id)
+    return render(request, 'product-details.html', {'product': product})
