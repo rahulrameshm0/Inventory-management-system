67c7fd714a864f7dacc98fcc66bdfcbf37d9f212
rahulrameshm0 <iamrahulm23@gmail.com>
2026-01-20 01:55:22 +0530

    code updated and pushed to the codebase

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,7 @@ const dashboardButton = document.querySelector('.btn-1');
 const form = document.getElementById('product-form');
 const deleteBtn = document.querySelector('.delete');
 const deleteForm = document.getElementById('delete-form');
-const upload_img = document.getElementById('images');
+const upload_img = document.getElementById('image');
 
 
 if(button){
@@ -49,7 +49,7 @@ function openPopup(btn = null) {
     document.getElementById('status').value = btn.dataset.status;
     document.getElementById('product_type').value = btn.dataset.type;
     document.getElementById('vendor_name').value = btn.dataset.vendor_name;
-    document.getElementById('img').value = btn.dataset.img;
+    document.getElementById('image').value = btn.dataset.images;
 }
 
 function closePopup(){   
